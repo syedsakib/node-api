3970bfec4cea923a038e0cc0722d86892cd3d6c7
syedsakib <sakibsyed01@gmail.com>
2020-09-28 08:37:25 +0600

    error & slugify & geocode

---
+++ middleware/error.js
@@ -0,0 +1,33 @@
+const ErrorResponse = require('../utils/errorResponse');
+
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+
+  console.log(err.stack.red);
+
+  //Mongoose bad ObjectID
+  if (err.name === 'CastError') {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  //Mongoose Duplicate key
+  if (err.code === 11000) {
+    const message = `Dupicate field value entered`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  //Mongoose validationError
+  if (err.name == 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || 'Server Error',
+  });
+};
+
+module.exports = errorHandler;
